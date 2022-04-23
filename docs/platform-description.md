1c95f240f2ab5acd06f3277c54298f4862325a6f
Intelix8996 <nikolat-repin201303@mail.ru>
2022-04-23 19:19:13 +0700

    Started writing documentation, updated scheme

--- docs/platform-description.md
+++ docs/platform-description.md
@@ -1,1 +1,16 @@
 # Universal Modular Platform based on Cdm-8 processor 
+
+# Platform Description
+
+# Overview
+# Hardware 
+## Basic setup
+## Expanding ROM
+### Mb interrupts here
+## Expanding RAM
+## Handling Interrupts
+## Devices description
+# Software
+## cocomake
+## vs code integration
+# Demonstration
--- include/devices.h
+++ include/devices.h
@@ -6,11 +6,14 @@
 #define DISP_A_ADDR             0xfa    // First (A) display address
 #define DISP_B_ADDR             0xfb    // Second (B) display address
 #define IR_BUFFER_ADDR          0xf0    // IR enable buffer address
-#define RANDOM_GEN_ADDR         0xf0    // Random number generator address
-#define IR_PAGE_REG_ADDR        0xf0    // IR page register address
-#define JOYSICK_ADDR            0xf0    // Joystick address
+#define RANDOM_GEN_ADDR         0xf1    // Random number generator address
+#define IR_PAGE_REG_ADDR        0xf2    // IR page register address
+#define JOYSICK_ADDR            0xf3    // Joystick address
 #define KEYPAD_ADDR             0xfc    // Keypad address
-#define TERMINAL_ADDR           0xf0    // Terminal and keybaord address
+#define TERMINAL_ADDR           0xf4    // Terminal and keybaord address
 #define MATH_COPROC_ADDR        0xf5    // Math coprocessor address
+#define USER_DISP_A_ADDR        0xf6    // First seven segment display address
+#define USER_DISP_B_ADDR        0xf7    // Second seven segment display address
+#define STATUS_DISP_ADDR        0xf9    // Status hex display address
 
 #endif
