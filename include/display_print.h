e593f66d864f625225191c454d06a27f05ee78f2
Intelix8996 <nikolat-repin201303@mail.ru>
2022-05-14 18:47:12 +0700

    implemented text output to screen

---
+++ include/display_print.h
@@ -0,0 +1,10 @@
+#ifndef DISPALY_PRINT_H
+#define DISPALY_PRINT_H
+
+#include <call.h>
+    
+#define DISP_PRINTC 0x00 // Entry point for printc function
+
+#define DISP_CODE   0x2d // Code section
+
+#endif
